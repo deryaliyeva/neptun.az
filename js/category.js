2aaaa1512cfb318be9eb6a0f4a09576c49c72426
deryaliyeva <dryaliyeva8@gmail.com>
2025-05-06 15:14:59 +0400

    all commits

--- js/category.js
+++ js/category.js
@@ -39,7 +39,7 @@ function showCard(data) {
     content.innerHTML = ""
     data.map(item => {
         content.innerHTML += `
-                            <article class="max-w-xs rounded-md bg-white max-h-full">
+                            <a href="/pages/details.htm" class="max-w-xs rounded-md bg-white max-h-full">
                                     <img src="${item.img[0]}" alt="" class="object-cover mx-auto w-32 rounded-t-md h-32 dark:bg-gray-500">
                                     <div class="flex flex-col justify-between p-6 space-y-8">
                                         <div class="space-y-2 text-center">
@@ -57,7 +57,7 @@ function showCard(data) {
                                             <i class="fa-solid text-[#ff8300] rounded-md p-1 fa-arrows-rotate hover:bg-[#ff8300] hover:text-white transition-all"></i>
                                         </div>
                                     </div>
-                            </article>
+                            </a>
                                 `
     })
 }
@@ -97,4 +97,10 @@ function handleBtn(x) {
                 <button onclick="clickButton(${i + 1})" type="button" title="${i + 1}" class="inline-flex items-center justify-center w-8 h-8 text-sm font-semibold border rounded shadow-md dark:bg-gray-50 border-[#ff8300]">${i + 1}</button>
         `
     })
+}
+window.row = function(){
+    content.classList.add('displayFlex')
+}
+window.col = function(){
+    content.classList.remove('displayFlex')
 }
